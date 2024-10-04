890e46947479a845fd9700ed3b291d57eb6e38ea
Luis Reinoso <LuisReinoso@users.noreply.github.com>
2024-10-03 22:09:07 -0500

    feat: setup electron

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"), // Define your preload.js if required
+      nodeIntegration: true, // Consider security best practices here
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:4200"); // URL of Angular dev server
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
